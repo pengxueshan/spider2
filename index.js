1d89653eb6c455ecbaee8d4856f3646633c6c18e
xueshanpeng <289159468@qq.com>
2015-12-12 00:22:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var url = require('url');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+var eventproxy = require('eventproxy');
+
+var targetUrl = 'http://www.baidu.com/';
+superagent.get(targetUrl)
+    .end(function(err, res){
+        console.log(err);
+    });
